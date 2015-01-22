8c0aee94c3d6848413b4f75dd057066a1b1f757c
Mikhail.Chernichenko <Mikhail.Chernichenko@billing.ru>
2015-01-22 19:13:59 +0300

    fix

--- lab-4-objects/src/main/java/org/billing/jlab/obj/App.java
+++ lab-4-objects/src/main/java/org/billing/jlab/obj/App.java
@@ -7,7 +7,14 @@ package org.billing.jlab.obj;
 public class App 
 {
     public static void main( String[] args ) {
+        Employer[] staff = new Employer[3];
+        staff[0] = new Employer("Имя работничка", 15000, "15.12.1987");
+        staff[1] = new Employer("Имя работничка_1", 15000, "15.12.1997");
+        staff[2] = new Employer();;
+
         Employer employer = new Employer("Имя работничка", 75000, "15.12.1987");
         System.out.println("Hello World!");
+
+        System.out.println(staff[2].getFinalName());
     }
 }
