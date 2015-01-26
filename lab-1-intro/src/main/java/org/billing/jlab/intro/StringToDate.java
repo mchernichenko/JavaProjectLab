fabcdfaefde3c88be1da1d1cdf8f04886e6f20d6
Mikhail.Chernichenko <Mikhail.Chernichenko@billing.ru>
2015-01-26 18:58:46 +0300

    fix

--- lab-1-intro/src/main/java/org/billing/jlab/intro/StringToDate.java
+++ lab-1-intro/src/main/java/org/billing/jlab/intro/StringToDate.java
@@ -1,17 +1,21 @@
 package org.billing.jlab.intro;
 
 import java.sql.Timestamp;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 /**
- * http://www.mkyong.com/java/how-to-convert-string-to-date-java/
- * http://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
- *
  * Преобразование текстовой даты (например, Timestamp) в объект Date и далее в Calendar
  * и наоборот преобразование даты в String нужного формата.
+ *
+ * Для форматирования даты и времени используется DateFormat и SimpledateFormat (подробнее см. Хорстмана стр.934)
+ *
+ * http://www.mkyong.com/java/how-to-convert-string-to-date-java/
+ * http://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
  *
  * Примеры форматов дат в виде строки:
  * "dd/MM/yyyy" - 07/06/2013
@@ -24,11 +28,11 @@ public class StringToDate {
 
     public static Date convertStringToDate(String strDate) {
         SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
-        String dateInString = "7-Jun-2013"; // = strDate
+        String strDate_1 = "7-Jun-2013"; // = strDate
 
         try {
 
-            Date date = formatter.parse(dateInString);
+            Date date = formatter.parse(strDate_1); // String -> Date
             System.out.println(date);                   // Fri Jun 07 00:00:00 MYT 2013
             System.out.println(formatter.format(date)); // 07-Jun-2013
 
@@ -44,7 +48,11 @@ public class StringToDate {
     }
 
     public static String convertCalendarToString(Calendar calendar) {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");  // Формат для TimeStamp
+
+        // задание предопределённого формата, относительно региона. Например: July 15, 2015
+       // DateFormat formatter = DateFormat.getDateInstance(DateFormat.LONG, Locale.getDefault());
+
         return formatter.format(calendar.getTime());
     }
 
