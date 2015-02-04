f46c81da2aa00f49035bdb8625302d4b4a845540
Mikhail.Chernichenko <Mikhail.Chernichenko@billing.ru>
2015-02-04 15:58:04 +0300

    fix lab-5-6

---
+++ lab-5-6-oop/src/main/java/org/billing/jlab/oop/App.java
@@ -0,0 +1,13 @@
+package org.billing.jlab.oop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
