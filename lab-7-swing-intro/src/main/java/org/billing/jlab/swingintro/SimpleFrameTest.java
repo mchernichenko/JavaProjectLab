f506f02e2a9dc73512c4179fee7a66556c31d893
Mikhail.Chernichenko <mchernichenko@mail.ru>
2015-04-13 01:04:24 +0300

    fix

--- lab-7-swing-intro/src/main/java/org/billing/jlab/swingintro/SimpleFrameTest.java
+++ lab-7-swing-intro/src/main/java/org/billing/jlab/swingintro/SimpleFrameTest.java
@@ -11,7 +11,9 @@ import java.awt.event.ActionListener;
  */
 public class SimpleFrameTest {
 
-    JButton button;
+    JButton colorButton;
+    SimpleFrame frame;
+    JLabel label;
 
     public static void main(String[] args) {
         SimpleFrameTest simpleFrameTest = new SimpleFrameTest();
@@ -24,19 +26,30 @@ public class SimpleFrameTest {
             public void run() {
 
                 // начинается создание GUI с создание фрейма, т.е. с new JFrame() или с класса наследника JFrame
-                SimpleFrame frame = new SimpleFrame();
+                frame = new SimpleFrame();
 
                 // создаём кнопку и регистрируем текущий класс в список слушателей. Слушатели должны реализовывать интерфейс ActionListener
                 // Объект является источником событий, если он имеет метод addБлаБлаБлаListener - это шаблон наименования метода,
                 // а в качестве аргумента интерфейс слушателя
-                button = new JButton("Надпись на кнопке");
-                button.addActionListener(new MyListener());
+                colorButton = new JButton("Изменить цвет");
+                colorButton.addActionListener(new ColorListener());
+
+                JButton labelButton = new JButton("Change Label");
+                labelButton.addActionListener(new LabelListener());
+
+                label = new JLabel("Label");
+
+                MyDrawPanel myDrawPanel = new MyDrawPanel();
 
                 // завершает работу программыы при закрытии окна, иначе оно будет висеть на экране вечно
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-                // добавляем кнопку на панель фрейма
-                frame.getContentPane().add(button);
+                // добавляем кнопку и личный виджет на панель фрейма, указывая область, куда будет добавлен виждет
+                // JFrame не позволяет добавлять в него объекты напрямую, нужно истопьзовать понель содержимого принадлежащую JFrame
+                frame.getContentPane().add(BorderLayout.SOUTH,colorButton);
+                frame.getContentPane().add(BorderLayout.CENTER,myDrawPanel);
+                frame.getContentPane().add(BorderLayout.EAST,labelButton);
+                frame.getContentPane().add(BorderLayout.WEST,label);
 
                 // по умолчанию фрейм не видим, требуется явно указать его видимость
                 frame.setVisible(true);
@@ -44,7 +57,7 @@ public class SimpleFrameTest {
         });
     }
 
-    class MyListener implements ActionListener {
+    class ColorListener implements ActionListener {
 
         /**
          * Метод обработки события нажатия кнопки
@@ -53,7 +66,15 @@ public class SimpleFrameTest {
          */
         @Override
         public void actionPerformed(ActionEvent e) {
-            button.setText("Опаньки");
+            //button.setText("Опаньки");
+            frame.repaint(); // перерисовка фрейма вызывает paintComponent для каждого виждета во фрейме
+        }
+    }
+
+    class LabelListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            label.setText("Опаньки\n" + Math.round(100 * Math.random()));
         }
     }
 }
@@ -68,5 +89,39 @@ class SimpleFrame extends JFrame {
     public SimpleFrame() {
         // присваиваем фрейму размер
         setSize(DAFAULT_WIDTH, DAFAULT_HEIGHT);
+    }
+}
+
+/**
+ * Создание личного виджета для рисования, который помещается на фрейм аналогично другим виджетам, например, кнопке.
+ * Graphics - Системная поверхность(холст) для рисования, где можно рисовать.
+ * Объект Graphics g на самом деле является экземпляром класса Graphics2G. Можно смето преобразовать тип, если требуется
+ * использовать методы из этого класса.
+ * paintComponent - метод, где должен располагаться весь графический код. Он никогда не вызывается самостоятельно!
+ * Можно только через принудительную перерисовку frame.repaint()
+ * Как правило его вызывает система, например, при изменении размеров фреймаи пр.
+ */
+class MyDrawPanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        int red = (int) (Math.random() * 255);
+        int green = (int) (Math.random() * 255);
+        int blue = (int) (Math.random() * 255);
+        Color startColor = new Color(red, green, blue);
+
+        red = (int) (Math.random() * 255);
+        green = (int) (Math.random() * 255);
+        blue = (int) (Math.random() * 255);
+        Color endColor = new Color(red, green, blue);
+
+        GradientPaint gradientPaint = new GradientPaint(70, 70, startColor, 150, 150, endColor);
+        g2d.setPaint(gradientPaint); // устанавливаем текущий цвет рисования (в даном случае градент)
+        g2d.fillOval(70,70,100,100); // рисуем овал текущим цветом
+
+        /*super.paintComponent(g);
+        g.setColor(Color.orange);
+        g.fillRect(20, 50, 100, 100);*/
     }
 }
