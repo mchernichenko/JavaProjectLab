db5f88073498da7e0f36066aed50012ef177360c
Mikhail.Chernichenko <Mikhail.Chernichenko@billing.ru>
2015-04-13 18:57:37 +0300

    fix

--- lab-7-swing-intro/src/main/java/org/billing/jlab/swingintro/SimpleFrameTest.java
+++ lab-7-swing-intro/src/main/java/org/billing/jlab/swingintro/SimpleFrameTest.java
@@ -5,6 +5,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static java.lang.Thread.sleep;
+
 /**
  * JFrame - объект, который представляет собой окно на экране в которое помещаются все остальные элементы GUI
  * JFrame по-разному выглядит в зависимости от платформы
@@ -14,6 +16,7 @@ public class SimpleFrameTest {
     JButton colorButton;
     SimpleFrame frame;
     JLabel label;
+    int x, y; // координаты эллипса
 
     public static void main(String[] args) {
         SimpleFrameTest simpleFrameTest = new SimpleFrameTest();
@@ -53,6 +56,20 @@ public class SimpleFrameTest {
 
                 // по умолчанию фрейм не видим, требуется явно указать его видимость
                 frame.setVisible(true);
+
+                for (int i = 0; i < 200; i++) {
+                    // меняем координаты эллипса
+                    y++;
+                    x++;
+                    myDrawPanel.repaint();  // перересовываем панель (т.е. вызываем каждый раз paintComponent)
+
+                    // немного замедляем процесс чтобы что-то увидеть
+            /*        try {
+                        sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }*/
+                }
             }
         });
     }
@@ -67,6 +84,8 @@ public class SimpleFrameTest {
         @Override
         public void actionPerformed(ActionEvent e) {
             //button.setText("Опаньки");
+            x++;
+            y++;
             frame.repaint(); // перерисовка фрейма вызывает paintComponent для каждого виждета во фрейме
         }
     }
@@ -77,14 +96,54 @@ public class SimpleFrameTest {
             label.setText("Опаньки\n" + Math.round(100 * Math.random()));
         }
     }
+
+    /**
+     * Панель для рисования недостаточно подходит для повторного использования, чтобы стать отдельным классом (поэтому делаем внутренним),
+     * ведь созданные на ней рисунки фактически предназначены для данного графического приложения
+     *
+     * Создание личного виджета для рисования, который помещается на фрейм аналогично другим виджетам, например, кнопке.
+     * Graphics - Системная поверхность(холст) для рисования, где можно рисовать.
+     * Объект Graphics g на самом деле является экземпляром класса Graphics2G. Можно смето преобразовать тип, если требуется
+     * использовать методы из этого класса.
+     * paintComponent - метод, где должен располагаться весь графический код. Он никогда не вызывается самостоятельно!
+     * Можно только через принудительную перерисовку frame.repaint()
+     * Как правило его вызывает система, например, при изменении размеров фреймаи пр.
+     */
+    class MyDrawPanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g) {
+
+            // установка цвета фона и закрашиваем прямойгольник а всю ширину панели.
+            g.setColor(Color.orange);
+            g.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+            // установка произвольных цветов для градиента
+            Graphics2D g2d = (Graphics2D) g;
+
+            int red = (int) (Math.random() * 255);
+            int green = (int) (Math.random() * 255);
+            int blue = (int) (Math.random() * 255);
+            Color startColor = new Color(red, green, blue);
+
+            red = (int) (Math.random() * 255);
+            green = (int) (Math.random() * 255);
+            blue = (int) (Math.random() * 255);
+            Color endColor = new Color(red, green, blue);
+
+            GradientPaint gradientPaint = new GradientPaint(x, y, startColor, x + 100, y + 100, endColor);
+            g2d.setPaint(gradientPaint); // устанавливаем текущий цвет рисования (в даном случае градент)
+            g2d.fillOval(x, y, 100, 100); // рисуем овал текущим цветом с диаметром 100
+
+        }
+    }
 }
 
 /**
- * Класс наследник JFrame для установки начальных параметров фрейма
+ *  Класс наследник JFrame для установки начальных параметров фрейма
  */
 class SimpleFrame extends JFrame {
-    private static final int DAFAULT_WIDTH = 300;
-    private static final int DAFAULT_HEIGHT = 200;
+    private static final int DAFAULT_WIDTH = 800;
+    private static final int DAFAULT_HEIGHT = 500;
 
     public SimpleFrame() {
         // присваиваем фрейму размер
@@ -92,36 +151,4 @@ class SimpleFrame extends JFrame {
     }
 }
 
-/**
- * Создание личного виджета для рисования, который помещается на фрейм аналогично другим виджетам, например, кнопке.
- * Graphics - Системная поверхность(холст) для рисования, где можно рисовать.
- * Объект Graphics g на самом деле является экземпляром класса Graphics2G. Можно смето преобразовать тип, если требуется
- * использовать методы из этого класса.
- * paintComponent - метод, где должен располагаться весь графический код. Он никогда не вызывается самостоятельно!
- * Можно только через принудительную перерисовку frame.repaint()
- * Как правило его вызывает система, например, при изменении размеров фреймаи пр.
- */
-class MyDrawPanel extends JPanel {
-    @Override
-    protected void paintComponent(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
 
-        int red = (int) (Math.random() * 255);
-        int green = (int) (Math.random() * 255);
-        int blue = (int) (Math.random() * 255);
-        Color startColor = new Color(red, green, blue);
-
-        red = (int) (Math.random() * 255);
-        green = (int) (Math.random() * 255);
-        blue = (int) (Math.random() * 255);
-        Color endColor = new Color(red, green, blue);
-
-        GradientPaint gradientPaint = new GradientPaint(70, 70, startColor, 150, 150, endColor);
-        g2d.setPaint(gradientPaint); // устанавливаем текущий цвет рисования (в даном случае градент)
-        g2d.fillOval(70,70,100,100); // рисуем овал текущим цветом
-
-        /*super.paintComponent(g);
-        g.setColor(Color.orange);
-        g.fillRect(20, 50, 100, 100);*/
-    }
-}
