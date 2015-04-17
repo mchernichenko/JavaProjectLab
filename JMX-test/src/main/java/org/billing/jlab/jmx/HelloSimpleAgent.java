210e99e4a271ec6c1ae3901521d1e57849d886de
Mikhail.Chernichenko <Mikhail.Chernichenko@billing.ru>
2015-04-17 12:40:55 +0300

    fix

--- JMX-test/src/main/java/org/billing/jlab/jmx/HelloSimpleAgent.java
+++ JMX-test/src/main/java/org/billing/jlab/jmx/HelloSimpleAgent.java
@@ -5,7 +5,9 @@ import javax.management.ObjectName;
 import java.lang.management.ManagementFactory;
 
 /**
- * создадим сервер и сделаем его локальным, т.е. он будет доступен только на нашей машине и с нашей машины.
+ * Агент JMX - место где регистрируются MBeans - объекты, управляющие определёнными ресурсами, которые они предлставляют
+ *
+ * Создадим сервер в котором регистрируются MBeans и сделаем его локальным, т.е. он будет доступен только на нашей машине и с нашей машины.
  * Самые главные стоки здесь
  *  1. Получение экземпляра MBeanServer: mbs = ManagementFactory.getPlatformMBeanServer();
  *     Интерфейс MBeanServer содержит немалое число методов для управления MBean’ами, которые на нем зарегистрированы.
@@ -26,8 +28,14 @@ public class HelloSimpleAgent
        // ObjectName helloName = null;
 
         try {
-            // И регистрируем его на платформе MBeanServer
+            // определяем имя объекта для экземпляра MBean.
+            // У каждого JMX MBean должно быть имя объекта соответствующее синтаксису, определенному спецификацией JMX,
+            // а именно, имя должно включать ДОМЕН, и СПИСОК ключевых СВОЙСТВ
+            // В нашем случае дамен это пакет, в котором содержится MBean: org.billing.jlab.jmx
+            // Ключевое свойство: тип объекта, как правило класс реализующий интерфейс MBean
             ObjectName helloName = new ObjectName("org.billing.jlab.jmx:type=SimpleAgent");
+
+            // регистрируем MBean c предопределённым именем на платформе MBeanServer
             mbs.registerMBean(helloBean, helloName);
         } catch(Exception e) {
             e.printStackTrace();
