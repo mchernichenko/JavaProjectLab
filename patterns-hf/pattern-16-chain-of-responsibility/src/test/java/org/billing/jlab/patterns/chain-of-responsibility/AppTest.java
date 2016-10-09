0e252221f41351061ba200ca2e1d2afb180abc98
Mikhail.Chernichenko <Mikhail.Chernichenko@billing.ru>
2016-10-09 18:15:54 +0300

    add

---
+++ patterns-hf/pattern-16-chain-of-responsibility/src/test/java/org/billing/jlab/patterns/chain-of-responsibility/AppTest.java
@@ -0,0 +1,29 @@
+package org.billing.jlab.patterns.iterator;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
