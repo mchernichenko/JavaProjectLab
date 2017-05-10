56b784dfd93ce1e72a3790dcc93885a5b42d6ed7
Mikhail.Chernichenko <Mikhail.Chernichenko@billing.ru>
2017-05-10 23:29:29 +0300

    ansible

--- ansible/3_jinja2_templating/run.sh
+++ ansible/3_jinja2_templating/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
- ansible-playbook -i hosts playbook_jinja_test.yml  # Запуск плейбука с hosts, расположенным в папке с плейбуком
+ ansible-playbook -i hosts playbook_jinja_test.yml  --tags "tg1" # Запуск плейбука с hosts, расположенным в папке с плейбуком
 # ansible-playbook -i hosts playbook_jinja_test.yml --tags "show_var"
 
 # ansible-playbook example.yml --tags "delete_file,copy_file"  # запуск конкретных теггированных тасков
