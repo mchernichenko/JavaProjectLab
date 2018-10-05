416f2596a24ca1284a3296c46fd806ea67b9b01c
Mikhail.Chernichenko <Mikhail.Chernichenko@nexign-systems.com>
2018-10-05 19:25:08 +0300

    fix state

---
+++ patterns-hf/pattern-10-state/src/main/java/org/billing/jlab/patterns/state/gumballstatewinner/State.java
@@ -0,0 +1,9 @@
+package org.billing.jlab.patterns.state.gumballstatewinner;
+
+public interface State {
+ 
+	public void insertQuarter();
+	public void ejectQuarter();
+	public void turnCrank();
+	public void dispense();
+}
